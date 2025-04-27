ed1082f5e38c3f75d6c0c98e9bcac7def9aec4de
Jdamour12 <jeandamourkubwimana0@gmail.com>
2025-04-27 11:36:15 +0200

    Created a javascript file for adding interactivity to my website

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function changeBackgroundColor(color){
+    let section = document.getElementById('coding-journey');
+    section.style.backgroundColor = color;
+}
+
+document.getElementById('intro').addEventListener(
+    (click),
+    () => changeBackgroundColor('#2ca02c')
+);
+
+document.getElementById(
+    (click),
+    () => changeBackgroundColor('#FF7F0E')
+);
+
+document.getElementById(
+    (click),
+    () => changeBackgroundColor('#9467BD')
+);
